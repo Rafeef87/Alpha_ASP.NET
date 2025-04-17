eea5e8195383ba460a0a2dcc7dc3aca85c416168
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-17 13:22:16 +0200

    Add project files.

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Route("admin")]
+public class AdminController : Controller
+{
+    [Route("members")]
+    public IActionResult Members()
+    {
+        return View();
+    }
+    [Route("clients")]
+    public IActionResult Clients()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult AddClient(AddClientForm form)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Clients");
+
+        return View();
+    }
+    [HttpPost]
+    public IActionResult EditClient(EditClientForm form)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Clients");
+
+        return View();
+    }
+}
---
+++ WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class ProjectsController : Controller
+{
+    [Route("projects")]
+    public IActionResult Projects()
+    {
+        return View();
+    }
+}
